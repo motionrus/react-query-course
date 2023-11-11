c72c0400449abd08ef460eda79746fc5c7c4b9d9
Тютин Руслан Ильдусович <rtiutin@mts.ru>
2023-11-12 02:11:57 +0300

    fixed

--- src/helpers/useUserData.js
+++ src/helpers/useUserData.js
@@ -3,7 +3,10 @@ import { useQuery } from "react-query";
 export function useUserData(userId) {
   const usersData = useQuery(
     ["users", userId],
-    () => userId && fetch(`/api/users/${userId}`).then((res) => res.json()),
+    () => fetch(`/api/users/${userId}`).then((res) => res.json()),
+    {
+      enabled: !!userId,
+    },
   );
   return usersData.data;
 }
